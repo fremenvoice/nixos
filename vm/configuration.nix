b1b3742b131bfd59f2a898b38f089f634d66cea9
Daniil Ivanov <openaifremen@gmail.com>
2025-05-28 15:54:03 +0400

    initial commit4

--- vm/configuration.nix
+++ vm/configuration.nix
@@ -2,8 +2,14 @@
 
 {
   imports = [ ./disko-config.nix ];
+
+  # GRUB bootloader (требуется для загрузки!)
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "/dev/sda" ];
+
   networking.hostName = "vm";
-  time.timeZone = "Europe/Moscow";
+  time.timeZone = "Europe/Samara";
+
   services.xserver = {
     enable = true;
     layout = "us,ru";
@@ -11,8 +17,10 @@
     displayManager.sddm.enable = true;
     desktopManager.plasma5.enable = false;
   };
+
   services.xserver.desktopManager.gnome.enable = false;
   programs.hyprland.enable = true;
+
   environment.systemPackages = with pkgs; [
     firefox
     konsole
@@ -20,12 +28,15 @@
     git
     wget
   ];
+
   users.users.fremen = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     password = "fremen";
   };
+
   security.sudo.wheelNeedsPassword = false;
+
   networking.networkmanager.enable = true;
   services.openssh.enable = true;
 }
